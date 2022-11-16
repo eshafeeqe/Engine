d28d63012cbb15bfb14285cf8750e449f76cdaf3
eshafeeqe <eshafeeqe@gmail.com>
2022-11-17 08:37:26 +1100

    First version of Event system Implementation

--- Engine/src/Application.cpp
+++ Engine/src/Application.cpp
@@ -1,6 +1,9 @@
 
 #include "Application.h"
 
+#include "Events/ApplicationEvent.h"
+#include "Log.h"
+
 namespace Engine
 {
  
@@ -12,6 +15,9 @@ namespace Engine
 
     void Application::run()
     {
+        WindowResizeEvent e(1280, 700);
+        EG_TRACE(e);
+
         while (true);
     }
 
---
+++ Engine/src/Events/ApplicationEvent.h
@@ -0,0 +1,87 @@
+#pragma once
+
+#include "Event.h"
+#include "../Core.h"
+
+#include "sstream"
+
+namespace Engine
+{
+
+    class ENGINE_API  WindowResizeEvent : public Event
+    {
+    
+    public: 
+
+        WindowResizeEvent(uint width, uint height)
+        :    m_Width(width), m_Height(height) {}
+
+        inline uint GetWidth()  const { return m_Width;}
+        inline uint GetHeight() const { return m_Height;}
+
+        std::string ToString() const override
+        {
+            std::stringstream ss;
+            ss << "WindowResizeEvent: " << m_Width <<" "<<m_Height;
+            return ss.str();
+        }
+
+        EVENT_CLASS_TYPE(WindowResize)
+        EVENT_CLASS_CATEGORY(EventCategoryApplication)
+    
+    private:
+        uint m_Width, m_Height; 
+
+    };
+
+    
+    class ENGINE_API   WindowCloseEvent : public Event
+    {
+    
+    public: 
+
+        WindowCloseEvent();
+        
+        EVENT_CLASS_TYPE(WindowClose)
+        EVENT_CLASS_CATEGORY(EventCategoryApplication)
+
+    };
+
+    class ENGINE_API   AppTickEvent : public Event
+    {
+    
+    public: 
+
+        AppTickEvent();
+        
+        EVENT_CLASS_TYPE(AppTick)
+        EVENT_CLASS_CATEGORY(EventCategoryApplication)
+
+    };
+
+    class ENGINE_API   AppUpdateEvent : public Event
+    {
+    
+    public: 
+
+        AppUpdateEvent();
+        
+        EVENT_CLASS_TYPE(AppUpdate)
+        EVENT_CLASS_CATEGORY(EventCategoryApplication)
+
+    };
+
+    class ENGINE_API   AppRenderEvent : public Event
+    {
+    
+    public: 
+
+        AppRenderEvent();
+        
+        EVENT_CLASS_TYPE(AppRender)
+        EVENT_CLASS_CATEGORY(EventCategoryApplication)
+
+    };
+
+
+}
