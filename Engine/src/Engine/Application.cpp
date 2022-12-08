13e73b221149092b8adcb32466d139caff408daa
eshafeeqe <eshafeeqe@gmail.com>
2022-12-09 08:25:06 +1100

    OpenGL first triangle
    Fixed a typo bug GL_FLAT instead of GL_FLOAT.

--- Engine/src/Engine/Application.cpp
+++ Engine/src/Engine/Application.cpp
@@ -19,27 +19,27 @@ namespace Engine
         m_ImGuiLayer = std::make_shared<ImGuiLayer>();
         PushOverlay(m_ImGuiLayer);
 
-        // glGenVertexArrays(1, &m_VertexArray);
-        // glBindVertexArray(m_VertexArray);
+        glGenVertexArrays(1, &m_VertexArray);
+        glBindVertexArray(m_VertexArray);
         
-        // glGenBuffers(1, &m_VertexBuffer);
-        // glBindBuffer(GL_ARRAY_BUFFER, m_VertexBuffer);
+        glGenBuffers(1, &m_VertexBuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, m_VertexBuffer);
 
-        // float vertices[3 *3] = {
-        //     -0.5f, -0.5f, 0.0f,
-        //      0.5f, -0.5f, 0.0f,
-        //      0.0f,  0.5f, 0.0f,
-        // };
+        float vertices[3 *3] = {
+            -0.5f, -0.5f, 0.0f,
+             0.5f, -0.5f, 0.0f,
+             0.0f,  0.5f, 0.0f,
+        };
 
-        // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); 
-        // glEnableVertexAttribArray(0); 
-        // glVertexAttribPointer(0, 3, GL_FLAT, GL_FALSE, 3* sizeof(float), nullptr); 
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); 
+        glEnableVertexAttribArray(0); 
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3* sizeof(float), nullptr); 
 
-        // glGenBuffers(1, &m_IndexBuffer); 
-        // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IndexBuffer);
+        glGenBuffers(1, &m_IndexBuffer); 
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IndexBuffer);
 
-        // uint indices[3] = {0, 1, 2};
-        // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+        uint indices[3] = {0, 1, 2};
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
         
     }
 
@@ -55,8 +55,7 @@ namespace Engine
     {
         EventDispatcher dispatcher(e);
         dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(onWindowClose));
-        //EG_CORE_TRACE("{0}", e);
-
+        
         for(auto it = m_LayerStack.end(); it != m_LayerStack.begin(); )
         {
             (*--it)->OnEvent(e);
@@ -72,8 +71,9 @@ namespace Engine
             glClearColor(0.1f, 0.1f, 0.1f ,1);
             glClear(GL_COLOR_BUFFER_BIT);
             
-            // glBindVertexArray(m_VertexArray);
-            // glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr); 
+            glBindVertexArray(m_VertexArray);
+            glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr); 
+            glBindVertexArray(0);
 
             for(auto& layer: m_LayerStack)
             {
