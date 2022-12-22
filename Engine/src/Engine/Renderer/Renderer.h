eb5f1d645b9e91b0742e89bad3b3f99eb3208970
eshafeeqe <eshafeeqe@gmail.com>
2022-12-22 18:27:04 +1100

    Transforms Episode 38

--- Engine/src/Engine/Renderer/Renderer.h
+++ Engine/src/Engine/Renderer/Renderer.h
@@ -16,7 +16,8 @@ namespace Engine
 
         static void BeginScene(OrthographicCamera& camera);
         static void EndScene();
-        static void Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray);
+        static void Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, 
+                           const glm::mat4& transform = glm::mat4(1.0));
         
         inline static RendererAPI::API GetAPI() {return RendererAPI::GetAPI();}
 
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,11 +1,12 @@
 #include <Engine.h>
 #include <imgui.h>
+#include "glm/gtc/matrix_transform.hpp"
 
 class ExampleLayer: public Engine::Layer
 {
 public:
     ExampleLayer()
-        : Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f ), m_CameraPosition(0.0f)
+        : Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f ), m_CameraPosition(0.0f), m_SquarePosition(0.0f)
     {
 
         m_VertexArray.reset(Engine::VertexArray::Create());
@@ -34,10 +35,10 @@ public:
         m_SquareVA.reset(Engine::VertexArray::Create());
 
         float squareVertices[3 * 4] = {
-            -0.75f, -0.75f, 0.0f, 
-             0.75f, -0.75f, 0.0f, 
-             0.75f,  0.75f, 0.0f,
-            -0.75f,  0.75f, 0.0f, 
+            -0.5f, -0.5f, 0.0f, 
+             0.5f, -0.5f, 0.0f, 
+             0.5f,  0.5f, 0.0f,
+            -0.5f,  0.5f, 0.0f, 
 
         };
 
@@ -64,12 +65,13 @@ public:
             out vec3 v_Position;
             out vec4 v_Color;
             uniform mat4 u_ViewProjection;
+            uniform mat4 u_Transform;
 
             void main()
             {
                 v_Position = a_Position;
                 v_Color = a_Color;
-                gl_Position = u_ViewProjection*vec4(a_Position, 1);
+                gl_Position = u_ViewProjection*u_Transform*vec4(a_Position, 1);
             }
 
         )";
@@ -97,13 +99,14 @@ public:
             layout(location = 0) in vec3 a_Position;
             
             uniform mat4 u_ViewProjection;
+            uniform mat4 u_Transform;
             
             out vec3 v_Position;
             
             void main()
             {
                 v_Position = a_Position;
-                gl_Position = u_ViewProjection*vec4(a_Position, 1);
+                gl_Position = u_ViewProjection*u_Transform*vec4(a_Position, 1);
             }
 
         )";
@@ -117,7 +120,7 @@ public:
 
             void main()
             {
-                color = vec4(v_Position*0.5 + 0.5, 0.1);
+                color = vec4(0.0, 0.0, 1.0, 1.0);
             }
 
         )"; 
@@ -133,20 +136,31 @@ public:
     {
 
         if(Engine::Input::IsKeyPressed(EG_KEY_LEFT))
-            m_CameraPosition.x += m_CameraMoveSpeed*ts;
-        else if(Engine::Input::IsKeyPressed(EG_KEY_RIGHT))
             m_CameraPosition.x -= m_CameraMoveSpeed*ts;
+        else if(Engine::Input::IsKeyPressed(EG_KEY_RIGHT))
+            m_CameraPosition.x += m_CameraMoveSpeed*ts;
 
         if(Engine::Input::IsKeyPressed(EG_KEY_UP))
             m_CameraPosition.y += m_CameraMoveSpeed*ts;
         else if(Engine::Input::IsKeyPressed(EG_KEY_DOWN))
             m_CameraPosition.y -= m_CameraMoveSpeed*ts;
 
+
         if(Engine::Input::IsKeyPressed(EG_KEY_A))
             m_CameraRotation += m_CameraRotationSpeed*ts;
         
         if(Engine::Input::IsKeyPressed(EG_KEY_D))
             m_CameraRotation -= m_CameraRotationSpeed*ts;
+
+        if(Engine::Input::IsKeyPressed(EG_KEY_J))
+            m_SquarePosition.x -= m_SquareMoveSpeed*ts;
+        else if(Engine::Input::IsKeyPressed(EG_KEY_L))
+            m_SquarePosition.x += m_SquareMoveSpeed*ts;
+
+        if(Engine::Input::IsKeyPressed(EG_KEY_I))
+            m_SquarePosition.y -= m_SquareMoveSpeed*ts;
+        else if(Engine::Input::IsKeyPressed(EG_KEY_K))
+            m_SquarePosition.y += m_SquareMoveSpeed*ts;
 
         Engine::RenderCommand::SetClearColor({0.1f, 0.1f, 0.1f, 1});
         Engine::RenderCommand::Clear();
@@ -155,8 +169,19 @@ public:
         m_Camera.SetRotation(m_CameraRotation);
     
         Engine::Renderer::BeginScene(m_Camera);
+        
+        glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1));
 
-        Engine::Renderer::Submit(m_BlueShader, m_SquareVA);
+        for(int y = 0; y < 20; y++)
+        {
+            for(int x = 0; x < 20; x++)
+            {
+                glm::vec3 pos(x* 0.11f, y*0.11f, 0.0f );
+                glm::mat4 transform = glm::translate(glm::mat4(1.0), pos)*scale;    
+
+                Engine::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+            }
+        }
 
         Engine::Renderer::Submit(m_Shader, m_VertexArray);
 
@@ -185,10 +210,13 @@ private:
         Engine::OrthographicCamera m_Camera;
         
         glm::vec3 m_CameraPosition;
-        float m_CameraMoveSpeed = 1.0f;
+        float m_CameraMoveSpeed = 5.0f;
         
         float m_CameraRotation = 0.0f;
         float m_CameraRotationSpeed = 30.0f;
+
+        glm::vec3 m_SquarePosition;
+        float m_SquareMoveSpeed = 1.0f;
 
 };
 
